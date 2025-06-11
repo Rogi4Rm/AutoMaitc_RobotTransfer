5c9d0a7e52d33d75b13cfdc02cd93329b2033056
djsy01 <dlsgh8884@naver.com>
2025-06-11 23:45:08 +0900

    Server setting

--- client/src/pages/Data.jsx
+++ client/src/pages/Data.jsx
@@ -19,7 +19,7 @@ export default function RogiArmDashboard() {
       .then((res) => res.json())
       .then((data) => setList(data))
       .catch((err) => console.error("API 호출 실패:", err));
-  }, [API_URL]);
+  }, []);
 
   // 날짜 클릭 시 해당 날짜의 박스 개수 데이터 가져오기
   const handleClick = async (date) => {
